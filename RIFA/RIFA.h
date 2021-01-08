8f674ca7cbd70c0b15ff5cc079ecf2317b4d2ada
valeaussie <valeaussie@hotmail.it>
2021-01-08 11:03:26 +1100

    uploading first files

---
+++ RIFA/RIFA.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+using namespace std;
+
+//DEFINITIONS
+
+int firstGen();
+
+
+#endif
+#pragma once
---
+++ RIFA/RIFAmodel.cpp
@@ -0,0 +1,21 @@
+#define _USE_MATH_DEFINES
+#include <random>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <map>
+#include <algorithm>
+#include <math.h>
+#include "RIFA.h"
+
+using namespace std;
+
+int main() {
+
+	firstGen();
+
+	
+	return 0;
+
+
+}
